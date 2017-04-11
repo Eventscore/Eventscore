5658e433821c5cbf0fa9554b77bfb5795b6aff3e
Edwin Brower <edwin.brower@gmail.com>
2017-04-10 22:52:00 -0700

    Add pie chart with sample data successfully

--- app/containers/Graph.js
+++ app/containers/Graph.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import ReactNative, {ART} from 'react-native';
+import PiePiece from './PiePiece';
 
 const {
   View,
@@ -15,6 +16,14 @@ const {
   Rectangle,
   Shape,
 } = ART;
+
+import * as scale from 'd3-scale';
+import * as shape from 'd3-shape';
+
+const d3 = {
+  scale,
+  shape,
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -31,30 +40,86 @@ const styles = StyleSheet.create({
   }
 });
 
-const Graph = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Oh Hai
-      </Text>
+const data = [
+  {'number': 8, 'name': 'Fun activities'},
+  {'number': 7, 'name': 'Dog'},
+  {'number': 16, 'name': 'Food'},
+  {'number': 23, 'name': 'Car'},
+  {'number': 42, 'name': 'Rent'},
+  {'number': 4, 'name': 'Misc'},
+];
 
-<Surface width={500} height={200}>
-    <Group x={300} y={100}> 
-      <Shape
-        d={'M-68.9646319937036,-29.476762610114324A75,75,0,0,1,-49.345310456503256,-56.48044206582762L-20.635195356782273,-21.775874553905552A30,30,0,0,0,-27.086713440010442,-12.896121704557451Z'}
-        stroke={'#2ca02c'}  // green line
-        strokeWidth={3}     
-      />
-    </Group>
-</Surface>
+const colors = [
+  '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+  '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'
+];
+
+// this._value = this._value.bind(this);
+//  _value(item) { 
+//   return item.number; 
+// }
 
 
-    </View>
-  );
-};
+        // d={'M-68.9646319937036,-29.476762610114324A75,75,0,0,1,-49.345310456503256,-56.48044206582762L-20.635195356782273,-21.775874553905552A30,30,0,0,0,-27.086713440010442,-12.896121704557451Z'}
+class Graph extends Component {
+  constructor(props) {
+    super(props);
+    this._value = this._value.bind(this);
+  }  
+  _value(item) { return item.number; }
+  _label(item) { return item.name; }
+  _color(index) { 
+    return colors[index % colors.length]; 
+  }
 
+  _createPieChart(index) {
+    let arcs = d3.shape.pie()
+      // .value(this._value)
+      .value(this._value)
+      (data);
+
+    let arc = d3.shape.arc()
+      .outerRadius(150 / 2)  // Radius of the pie 
+      .padAngle(.05)    // Angle between sections
+      .innerRadius(30);  // Inner radius: to create a donut or pie
+      // (arcs[index]);
+
+    let arcData = arcs[index];
+    let path = arc(arcData);
+    return path;
+  }
+
+  // console.log('what is paths even anyway', arcs);
+  // console.log('this is the path', path);
+  // this._value = 
+          // key={index}
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Oh Hai
+        </Text>
+
+        <Surface width={200} height={500}>
+        <Group x={95} y={95}> 
+        {
+          data.map( (item, index) => 
+            // {console.log(item)}
+            (<PiePiece
+              key={index}
+              color={this._color(index)}
+              d={() => this._createPieChart(index)}  
+            />)
+          )
+        }
+        </Group>
+        </Surface>
+      </View>
+    );
+  }
+
+}
+
+              // color={colors[index]}
 export default connect(({routes}) => ({routes}))(Graph);
-
-
-
 
