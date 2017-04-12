faa744350fba844016823ebf7d8f91b05ba55e6c
Edwin Brower <edwin.brower@gmail.com>
2017-04-12 12:02:04 -0700

    Render aster plot on page

--- app/containers/Graph.js
+++ app/containers/Graph.js
@@ -35,37 +35,51 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    // margin: 10,
     color: '#ffffff',
   }
 });
 
 const data = [
-  {'number': 8, 'name': 'Spotify Artist Ranking'},
-  {'number': 7, 'name': 'Spotify Play Count'},
-  {'number': 16, 'name': 'SeatGeek Score'},
-  {'number': 23, 'name': 'iamjasonkuo internet crawling'},
-  {'number': 42, 'name': 'Beyonce test'},
-  {'number': 4, 'name': 'Misc'},
+  {'weight': 1, 'name': 'Spotify Artist Ranking', 'score': 60},
+  {'weight': 1, 'name': 'Spotify Play Count', 'score': 90},
+  {'weight': 1.5, 'name': 'SeatGeek Score', 'score': 75},
+  {'weight': 1, 'name': 'iamjasonkuo internet crawling', 'score': 40},
+  {'weight': 2, 'name': 'Beyonce test', 'score': 100},
+  {'weight': .5, 'name': 'Misc', 'score': 80},
 ];
+
+const eventScore = 
+  Math.round(
+  data.reduce(function(a, b) {
+    return a + (b.score * b.weight); 
+  }, 0) / 
+  data.reduce(function(a, b) { 
+    return a + b.weight; 
+  }, 0)
+  );
+
+// const data = [
+//   {'number': 8, 'name': 'Spotify Artist Ranking'},
+//   {'number': 7, 'name': 'Spotify Play Count'},
+//   {'number': 16, 'name': 'SeatGeek Score'},
+//   {'number': 23, 'name': 'iamjasonkuo internet crawling'},
+//   {'number': 42, 'name': 'Beyonce test'},
+//   {'number': 4, 'name': 'Misc'},
+// ];
 
 const colors = [
   '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
   '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'
 ];
 
-// this._value = this._value.bind(this);
-//  _value(item) { 
-//   return item.number; 
-// }
-
 // delete these later // change to props;
 const pieWidth = 150;
 const pieHeight = 150;
 const margin = 20;
-const width = 200;
+const width = 300;
 const height = 500;
-        // d={'M-68.9646319937036,-29.476762610114324A75,75,0,0,1,-49.345310456503256,-56.48044206582762L-20.635195356782273,-21.775874553905552A30,30,0,0,0,-27.086713440010442,-12.896121704557451Z'}
+
 class Graph extends Component {
   constructor(props) {
     super(props);
@@ -73,31 +87,37 @@ class Graph extends Component {
     this._label = this._label.bind(this);
     this._color = this._color.bind(this);
     this._createPieChart = this._createPieChart.bind(this);
-    // this._selectedPieItem = this._onPieItemSelected.bind(this);
+    this._onPieItemSelected = this._onPieItemSelected.bind(this);
   }  
-  _value(item) { return item.number; }
+  _value(item) { return item.weight; }
+  // _value(item) { return 100/data.length; }
   _label(item) { return item.name; }
   _color(index) { 
     return colors[index % colors.length]; 
   }
 
-  _createPieChart(index) {
+  _createPieChart(index, item) {
 
-
+    console.log('val', this._value);
     let arcs = d3.shape.pie()
       // .value(this._value)
       .value(this._value)
       (data);
 
     let arc = d3.shape.arc()
-      .outerRadius(150 / 2)  // Radius of the pie 
+      .outerRadius((pieWidth / 2)*item.score/100)  // Radius of the pie 
       .padAngle(.05)    // Angle between sections
-      .innerRadius(30);  // Inner radius: to create a donut or pie
+      .innerRadius(20);  // Inner radius: to create a donut or pie
       // (arcs[index]);
 
     let arcData = arcs[index];
     let path = arc(arcData);
     return path;
+  }
+
+  _onPieItemSelected(index) {
+    this.setState({...this.state, highlightedIndex: index});
+    this.props.onItemSelected(index);
   }
 
   // console.log('what is paths even anyway', arcs);
@@ -108,24 +128,27 @@ class Graph extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Oh Hai
+          Score
         </Text>
 
         <Surface width={width} height={height}>
-        <Group x={(pieWidth / 2) + margin} y={(pieWidth / 2) + margin}> 
+        <Group x={width/2} y={(pieWidth / 2)}> 
         {
           data.map( (item, index) => 
             // {console.log(item)}
             (<PiePiece
               key={index}
               color={this._color(index)}
-              d={() => this._createPieChart(index)}  
+              d={() => this._createPieChart(index, item)}  
             />)
           )
         }
         </Group>
         </Surface>
-        <View style={{position: 'absolute', top: 20 * margin, left: 2 * margin + pieWidth}}>
+        <View style={{position: 'absolute', top: margin + pieHeight/2 - 5, left: width/2 - 12}}>
+          <Text style={[styles.welcome, {}]}>{eventScore}</Text>
+        </View>
+        <View style={{position: 'absolute', top: 10 * margin, left: 2 * margin /* + pieWidth*/}}>
           {
             data.map( (item, index) =>
             // {
@@ -133,7 +156,7 @@ class Graph extends Component {
               
                 (<TouchableWithoutFeedback key={index} onPress={() => this._onPieItemSelected(index)}>
                   <View>
-                    <Text style={[styles.label, {color: this._color(index), fontWeight: 'normal'}]}>{this._label(item)}: {this._value(item)}%</Text>
+                    <Text style={[styles.label, {color: this._color(index), fontWeight: 'normal'}]}>{this._label(item)}: {item.score}%</Text>
                   </View>
                 </TouchableWithoutFeedback>
               )
@@ -147,21 +170,5 @@ class Graph extends Component {
 
 }
 
-          // above works. below was old
-          // {
-          //   data.map( (item, index) =>
-          //   {
-          //     // var fontWeight = this.state.highlightedIndex === index ? 'bold' : 'normal';
-          //     // return (
-          //       <TouchableWithoutFeedback key={index} onPress={() => this._onPieItemSelected(index)}>
-          //         <View>
-          //           <Text style={[styles.label, {color: this._color(index), fontWeight: fontWeight}]}>{this._label(item)}: {this._value(item)}%</Text>
-          //         </View>
-          //       </TouchableWithoutFeedback>
-          //     // );
-          //   });
-          // }
-
-              // color={colors[index]}
 export default connect(({routes}) => ({routes}))(Graph);
 
