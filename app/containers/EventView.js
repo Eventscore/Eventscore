a6f48adde16fc6a1a04889a8f5ee5320e45960a7
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-04-15 13:10:05 -0700

    (feat) Complete finishing touches for event styling

--- app/containers/EventView.js
+++ app/containers/EventView.js
@@ -36,23 +36,18 @@ class EventView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button
-        onPress={this.goBackToEventList}
-        title="Back"
-        color="#841584"
-        />
-        <Text style={{textAlign: 'center'}}>{this.props.eventsReducers.currEvent._id}</Text>
+        <TouchableHighlight onPress={this.goBackToEventList}>
+            <Text style={styles.backButton}>Back To Events!</Text>
+        </TouchableHighlight>
         <View style={styles.eventContainer}>
           <View style={styles.dateBox}>
             <Text style={styles.weekday}>Wed</Text>
             <Text style={styles.date}>Nov 12</Text>
           </View>
-          <View>
+          <View style={styles.eventInfo}>
             <Text style={styles.artist}>{this.props.eventsReducers.currEvent.artists[0].name}</Text>
             <Text style={styles.headline}>{this.props.eventsReducers.currEvent.name}</Text>
-            <View style={styles.timeVenue}>
-              <Text>8:00PM @ Wrigley Field</Text>
-            </View>
+            <Text style={styles.timeVenue}>8:00PM @ Wrigley Field</Text>
             <Text style={styles.location}>Chicago, IL</Text>
           </View>
           <View style={styles.scoreBox}>
@@ -67,36 +62,52 @@ class EventView extends Component {
 }
 
 const styles = StyleSheet.create({
+  backButton: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#333333',
+    margin: 5
+  },
   container: {
+    // backgroundColor: '#4682B4',
+    // flex: 1,
+    justifyContent: 'center',
     display: 'flex',
-    flex: 1,
-    paddingTop: 22
+    paddingTop: 22,
+    paddingBottom: 50
   },
   eventContainer: {
-    borderWidth: 0,
-    margin: 0.5,
     display: 'flex',
     flexDirection: 'row',
-    // alignItems: 'center',
-    // backgroundColor: '#4682B4',
+    alignItems: 'center',
+    backgroundColor: '#EEE',
+    // flex: 1,
     justifyContent: 'space-around',
-    height: 75
+    // height: 80,
+    // borderBottomWidth: 0.3,
+    padding: 3,
+    margin: 1.5,
+    // marginBottom: 3
   },
   dateBox:{
     display: 'flex',
     justifyContent: 'center',
+    flex: 0.5
   },
-  timeVenue:{
+  eventInfo:{
     display: 'flex',
-    flexDirection: 'row',
+    justifyContent: 'center',
+    flex: 1
   },
   scoreBox:{
     display: 'flex',
     justifyContent: 'center',
+    flex: 0.5
   },
   weekday:{
     textAlign: 'center',
-    fontSize: 20
+    fontSize: 18
   },
   date:{
     textAlign: 'center',
@@ -106,7 +117,12 @@ const styles = StyleSheet.create({
   },
   headline:{
     textAlign: 'center',
-    fontSize: 20
+    // flex: 1,
+    fontSize: 18
+  },
+  timeVenue:{
+    textAlign: 'center',
+    fontSize: 12
   },
   location:{
     textAlign: 'center',
@@ -117,7 +133,7 @@ const styles = StyleSheet.create({
   },
   scoreNumber:{
     textAlign: 'center',
-    fontSize: 20
+    fontSize: 18
   }
 });
 
