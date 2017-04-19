db09b8c887828a0dc978e059d08715e337459def
Edwin Brower <edwin.brower@gmail.com>
2017-04-18 19:02:52 -0700

    Reset initial to landing page

--- app/index.js
+++ app/index.js
@@ -30,10 +30,10 @@ import EventListMap from './containers/EventListMap';
 
 const scenes = Actions.create(
   <Scene key="root">
-    <Scene key="landing" direction="right" component={Landing} title="Landing" hideNavBar />    
+    <Scene key="landing" direction="right" component={Landing} title="Landing" hideNavBar initial/>    
     <Scene key="login" direction="right" component={Login} title="Login" hideNavBar />
     <Scene key="signup" direction="left" component={Signup} title="Sign Up" hideNavBar />
-    <Scene key="home" component={Home} title="Home" hideNavBar initial/>
+    <Scene key="home" component={Home} title="Home" hideNavBar />
     <Scene key="filter" component={Filter} title="Search" hideNavBar />
     <Scene key="event" component={EventList} title="Events" hideNavBar />
     <Scene key="eventview" component={EventView} hideNavBar />
